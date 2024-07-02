bbc3654e20376819330b13da122b6dbe2b00b779
Martí Espinosa <martiespinosa99@gmail.com>
2024-07-02 15:35:35 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Martí Espinosa Farran on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
